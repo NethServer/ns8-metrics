57383dfad1c7207cce5e0ddff3bf8e80711e90d0
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-02-20 16:32:41 +0100

    fixes

--- build-images.sh
+++ build-images.sh
@@ -22,7 +22,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.max-per-node=1" \
     --label='org.nethserver.flags=core_module' \
-    --label="org.nethserver.images=quay.io/prometheus/prometheus:v3.2.0 quay.io/prometheus/alertmanager:v0.28.0 ghcr.io/nethserver/node-monitor:latest" \
+    --label="org.nethserver.images=quay.io/prometheus/prometheus:v3.2.0 quay.io/prometheus/alertmanager:v0.28.0 ghcr.io/nethserver/metrics-node-monitor:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
